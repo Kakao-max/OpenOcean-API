7991878349a2580f5d55566220b76660dcd7847f
Kakao-max <angrybird4837@gmail.com>
2024-05-24 07:40:48 -0400

    completed

--- app/controller/middleWare.ts
+++ app/controller/middleWare.ts
@@ -86,7 +86,11 @@ export default class MiddleWareController extends Controller {
   /**
    * transfer
    */
-  
+  public async transfer() {
+    const { ctx } = this;
+    logger.error('dddd');
+    ctx.body = await ctx.service.middleWare.transfer(ctx.request.query);
+  }
 
   
 }
